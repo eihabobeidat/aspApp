09878417d8c8dd0d93365c472389b4ff5b6fa28b
e.obeidat <e.obeidat.ext@tahaluf.ae>
2022-11-02 20:09:46 +0300

    base setup, test api

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{identity}")]
+        public async Task<ActionResult<AppUser>> GetUser(int identity)
+        {
+            return await _context.Users.FindAsync(identity);
+        }
+
+
+    }
+}
